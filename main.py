6ad35b45ff8b871819588d15d2d7923f1457448f
Căițatu Dragoș <88626764+TheApolloCode@users.noreply.github.com>
2021-09-05 16:54:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 # EN:Tic Tac Toe
 # RO:X si 0
 
+tie=0
 
 v = ["*", "*", "*",
      "*", "*", "*",
@@ -43,9 +44,11 @@ while True:
     elif (v[2] == v[4] == v[6] == "x" or v[2] == v[4] == v[6] == "0"):
         print(v[2] + " wins")
         break
+    else:
+         straightLine=False
 
     # RO:Linie orizontala,EN:Horizontal line
-    elif (v[0] == v[1] == v[2] == "x" or v[0] == v[1] == v[2] == "0"):
+    if (v[0] == v[1] == v[2] == "x" or v[0] == v[1] == v[2] == "0"):
         print(v[0] + " wins")
         break
     elif (v[3] == v[4] == v[5] == "x" or v[3] == v[4] == v[5] == "0"):
@@ -54,10 +57,12 @@ while True:
     elif (v[6] == v[7] == v[8] == "x" or v[6] == v[7] == v[8] == "0"):
         print(v[6] + " wins")
         break
+    else:
+        horizontalLine=False
 
 
     # RO:Linie verticala,EN:Vertical line
-    elif (v[0] == v[3] == v[6] == "x" or [0] == v[3] == v[6] == "0"):
+    if (v[0] == v[3] == v[6] == "x" or [0] == v[3] == v[6] == "0"):
         print(v[0] + " wins")
         break
     elif (v[1] == v[4] == v[7] == "x" or v[1] == v[4] == v[7] == "0"):
@@ -66,6 +71,8 @@ while True:
     elif (v[2] == v[5] == v[8] == "x" or v[2] == v[5] == v[8] == "0"):
         print(v[2] + " wins")
         break
+    else:
+        verticalLine=False
 
 
     while player:
@@ -75,6 +82,14 @@ while True:
              v[position] = playerChoice
              player = False
              printBoard()
+             for i in range(0, 9):
+                 if v[i] == "x" or v[i] == "0":
+                     tie += 1
+    if tie == 9 and verticalLine==False and horizontalLine==False and straightLine==False:
+                 print("It's a tie")
+                 break
+    else:
+        tie=0
 
     '''
     EN:Player turn,the if statement verifies if the position can be occupied(x!=x,x!=0,x!=*)
@@ -87,11 +102,26 @@ while True:
           v[position] = computerChoice
           player = True
           printBoard()
-
-
+          for i in range(0, 9):
+              if v[i] == "x" or v[i] == "0":
+                  tie += 1
+    if tie == 9 and verticalLine==False and horizontalLine==False and straightLine==False:
+              print("It's a tie")
+              break
+    else:
+        tie=0
 '''
 EN:Computer random turn(can be improved with the implementation of the minimax AI algorithm)
 Does the same if check that happens above to verifiy if the move is possible 
 RO:Tura aleatorie a calculatorului(poate fi imbunatatita cu implementarea algoritmului de 
    inteligenta artificiala minimax)
-   Face aceeasi verificare care se intampla si mai sus,pentru a verifica daca mutarea este posibila'''
+   Face aceeasi verificare care se intampla si mai sus,pentru a verifica daca mutarea este posibila
+'''
+
+'''
+5.09.2021 V:1.01
+EN:Updates i did to my code after the first version I published:Added a tie checker,and a print command 
+to correctly show it.Game no longer runs in an infinite loop when there's a tie.
+RO:Actualizare pe care am facut-o codului meu dupa prima versiune:Am adauga un verificator de remiza si o
+comanda pentru a o afisa corect.Jocul nu mai merge intr-o bucla infinita cand este remiza.
+'''
